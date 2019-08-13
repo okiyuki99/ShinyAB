94647d42e05b7423fb5ae3202cb38c2e8d507c50
gingi99 <bigtree3101@gmail.com>
2019-08-13 19:47:48 +0900

    update renv

--- global.R
+++ global.R
@@ -1,5 +1,20 @@
+# Cleaning ----
+detach_all <- function() {
+  basic.pkg <- c("package:stats", "package:graphics", "package:grDevices", 
+                 "package:utils", "package:datasets", "package:methods", "package:base")
+  
+  pkg.list <- search()[ifelse(unlist(gregexpr("package:", search())) == 1 ,TRUE, FALSE)]
+  
+  pkg.list <- setdiff(pkg.list, basic.pkg)
+  
+  lapply(pkg.list, detach, character.only = TRUE)
+}
+detach_all()
+rm(list = ls())
+
+
 library(pacman)
-pacman::p_load(dplyr, tidyr, knitr, kableExtra, plotly, crosstalk, selectr, testthat)
+pacman::p_load(dplyr, knitr, kableExtra, plotly, testthat)
 pacman::p_load(shiny, shinydashboard, tippy, dashboardthemes, shinycssloaders, shinytest)
 
 source("utils/data.R")
