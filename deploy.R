2953949e6b62aaf519f31f69e4d3bdb7277cb8af
gingi99 <bigtree3101@gmail.com>
2019-08-14 21:12:33 +0900

    update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,4 +1,4 @@
 # Ref : https://docs.rstudio.com/shinyapps.io/getting-started.html#deploying-applications
 library(rsconnect)
 rsconnect::setAccountInfo(name = Sys.getenv("RS_NAME"), token = Sys.getenv("RS_TOKEN"), secret = Sys.getenv("RS_SECRET"))
-deployApp()
+rsconnect::deployApp(".", launch.browser = F, forceUpdate = T)
